aef34123d923046d5e9a4c554d0263a0f5ad1442
Nazar Yaremii <n.yaremiy791@gmail.com>
2022-12-12 23:44:24 +0200

    Clear full

--- client/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import qrcodeSlice from './../features/qrcode/qrcodeSlice';
-
-export const store = configureStore({
-  reducer: {
-    qrcode: qrcodeSlice
-  }
-});
